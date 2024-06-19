3e407f0d1983b785a323e29dd056cd8533d04d9b
kevinassis08 <kevindebarbadeassis@gmail.com>
2024-06-18 21:57:33 -0300

    primeiro commit no github

---
+++ aula1.php
@@ -0,0 +1,35 @@
+<?php
+/* aula de numero 1 dia 18-06-2024
+turma bg05000
+conteudos: echo, comentarios, variaveis
+por: kevin assis
+
+
+// este é um comentário do código, não sera exibido em tela.
+echo "buenass";
+
+# outro tipo de comentario
+
+/* mais um tipo de comentario */
+
+$texto = "<br>pequeno texto que voce esta lendo sobre o livro do menino maluquinho!";
+echo $texto;
+
+
+$outraVariavel = "Outro grande texto"; // Aqui é um comentario
+echo $outraVariavel;
+echo "outro texto :<br>primeira linha<br>segunda linha<br>";
+echo "<br> calculos matematicos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-7;
+echo "<br>";
+echo 99*180;
+echo "<br>";
+echo 100-43;
+echo "<br>";
+echo 4%2;
+
+
+
+?>
