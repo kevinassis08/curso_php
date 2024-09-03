a773899f63243d5f3c722a1f6aab6a6be4f3d71e
kevinassis08 <kevindebarbadeassis@gmail.com>
2024-09-03 19:16:24 -0300

    aula10.php

--- aula10.php
+++ aula10.php
@@ -85,6 +85,118 @@ echo "$operacao = $valor, Extrato: $saldoAtual<br>";*/
 
  $cpfValido = isValidCPF($cpf); // true = 1, false =
 
+function addMaskCPF($cpf){
+    $cpfMascarado = "";
+    $maskCPF = "###.###.###-##";
+
+    $a=0;
+    
+    for ($i=0; $i <= strlen($maskCPF); $i++){
+            
+        if($maskCPF[$i] == '#'){    
+            $cpfMascarado .= $cpf[$a]; // 056.552.020-26
+            $a++;
+        } else {
+            $cpfMascarado .= $maskCPF[$i]; // 056.552.020-
+        }
+    
+    }
+
+    return $cpfMascarado;
+}
+
+$cpfMascarado = addMaskCPF($cpf);
+echo $cpfMascarado;
+
+$cpf = "05655202026";
+
+$cpfvalido = validcpf($cpf);
+$cpf = preg_replace('/[^0-9]/', '', $cpf);
+
+
+if($cpfvalido){
+    echo "O CPF: $cpf é valido.";
+}else {
+    echo "O CPF: $cpf é invalido.";
+}
+ 
+
+
+                               //CNPJ
+
+function validCnpj($cnpj) {
+    // Remove caracteres não numéricos
+    $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+
+    // Verifica se o CNPJ tem 14 dígitos
+    if (strlen($cnpj) != 14) {
+        return false;
+    }
+
+    // Verifica se todos os dígitos são iguais
+    if (preg_match('/(\d)\1{13}/', $cnpj)) {
+        return false;
+    }
+
+    // Calcula o primeiro dígito verificador
+    $soma = 0;
+    $multiplicador = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    for ($i = 0; $i < 12; $i++) {
+        $soma += $cnpj[$i] * $multiplicador[$i];
+    }
+    $resto = $soma % 11;
+    $digito1 = ($resto < 2) ? 0 : 11 - $resto;
+
+    if ($cnpj[12] != $digito1) {
+        return false;
+    }
+
+function addMaskCNPJ($cnpj){
+    $cnpjMascarado= "";
+    $maskCPNJ = "##.###.###/####-##";
+    $c = 0;
+    
+    for ($i=0; $i <= strlen($maskCPNJ); $i++){
+            
+        if($maskCPNJ[$i] == '#'){    
+            $cnpjMascarado .= $cnpj[$c];
+            $c++;
+        } else {
+            $cnpjMascarado .= $maskCPNJ[$i]; 
+        }
+    
+    }
+
+    return $cnpjMascarado;
+}
+
+
+
+    // Calcula o segundo dígito verificador
+    $soma = 0;
+    $multiplicador = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    for ($i = 0; $i < 13; $i++) {
+        $soma += $cnpj[$i] * $multiplicador[$i];
+    }
+    $resto = $soma % 11;
+    $digito2 = ($resto < 2) ? 0 : 11 - $resto;
+
+    return $cnpj[13] == $digito2;
+}
+
+$maskCPNJ = "##.###.###/####-##";
+$cnpj = '11.444.777/0001-61'; //'11.444.777/0001-61'
+$cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+
+$cnpjvalido = validcnpj($cnpj);
+
+
+
+if($cnpjvalido) {
+    echo "<br>O CNPJ: $cnpj é valido.";
+} else {
+    echo "<br>O CNPJ: $cnpj é inválido";
+}
  if ($cpfValido) {
     echo "O CPF: $cpf é Valido";
  }else{
